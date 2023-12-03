fbf4087e3b643444493867cfb0f27d5340b61979
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2023-12-03 09:59:06 +0530

    Bug Fixes

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ app=Flask(__name__)
  
  
 
-boost=pickle.load(open('models/randomforest.pkl','rb'))
-standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
+model=pickle.load(open('models/randomforestn.pkl','rb'))
+standard_scaler=pickle.load(open('models/scalern.pkl','rb'))
 
 @app.route("/")
 def index():
@@ -46,16 +46,19 @@ def predict():
         pay5=int(request.form.get('payAmtMay'))
         pay6=int(request.form.get('payAmtApr'))
 
+       
+
         new_data=[[credit_limit, gender,education, marriage, age, repay1,repay2,repay3,repay4,repay5,repay6, bill1, bill2, bill3, bill4, bill5, bill6, pay1, pay2, pay3, pay4, pay5, pay6]]
+        print(new_data)
         scaled_data = standard_scaler.transform(new_data)
-        result=boost.predict(scaled_data)
+        result=model.predict(scaled_data)
         print(result)
         prediction_result = result[0]
         if prediction_result==0 :
             return render_template('nodefault.html')
         
         else:
-            render_template('decreaselimit.html')
+            return render_template('decreaselimit.html')
 
     else:
         return render_template('index.html')
